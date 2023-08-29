cd5c30ca960bd8183b623fc0232c05de5c55ea0c
Swarnkar, K Divya <k-divya.swarnkar@capgemini.com>
2023-08-29 11:51:11 +0530

    PageNotFound Wildcards pathMatch Routing Angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
+{path:'',redirectTo:'/home',pathMatch:'full'},
 {path:'home',component:HomeComponent},
 {path:'about',component:AboutComponent},
-{path:'contact',component:ContactComponent}
+{path:'contact',component:ContactComponent},
+{path:'**',component:PageNotFoundComponent}
 ];
 
 @NgModule({
